c9d762ca6237f3107f61502ad08dd45b05e367d2
cybersale26-design <cybersale26@gmail.com>
2026-06-23 14:49:52 +0530

    Delete layout.tsx

--- layout.tsx
+++
@@ -1,1 +0,0 @@
-import "./globals.css"; export const metadata={title:"Digital Chor Bazaar"}; export default function RootLayout({children}:{children:React.ReactNode}){return <html><body>{children}</body></html>}
